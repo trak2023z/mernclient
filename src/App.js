5338581c9a26b50055a9e693d5f38248abb7ae04
trak2023z <113351512+trak2023z@users.noreply.github.com>
2023-02-28 16:58:06 +0100

    c1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */ 
 
+import React, {useState, useEffect} from 'react';
+import FileUploadScreen from './FileUploadScreen';
+import {getSingleFiles, getMultipleFiles} from './data/api';
+import axios from 'axios'
+ 
 function App() {
+  const [singleFiles, setSingleFiles] = useState([]);
+  const [multipleFiles, setMultipleFiles] = useState([]);
+
+  const getSingleFileslist = async () => {
+    try {
+        const fileslist = await getSingleFiles();
+        setSingleFiles(fileslist);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const getMultipleFilesList = async () => {
+    try {
+        const fileslist = await getMultipleFiles();
+        setMultipleFiles(fileslist);
+        console.log(multipleFiles);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleDeleteImage = async (id)=>{
+      await axios.delete('https://localhost/api/' + id)
+      setSingleFiles(prev=>prev.filter(file=>file._id !== id))
+  }
+
+  useEffect(() => {
+    getSingleFileslist();
+    getMultipleFilesList();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="container">
+          <FileUploadScreen getsingle={() => getSingleFileslist()} getMultiple={() => getMultipleFilesList()}/>
+       </div> 
+       <div className="container-fluid mt-5">
+         <div className="row">
+           <div className="col-6">
+             <h4 className="text-success font-weight-bold">Single Files List</h4>
+             <div className="row">
+                {singleFiles.map((file, index) => 
+                  <div className="col-6">
+                    <div className="card mb-2 border-0 p-0">
+                      <img src={`https://localhost${file.filePath}`} height="200" className="card-img-top img-responsive" alt="img"/>
+                      <button size="small" variant="contained" color="secondary" onClick={() => handleDeleteImage(file._id)}>Delete</button>
+                      </div>
+                  </div>
+                )}
+             </div>
+           </div>
+           <div className="col-6">
+             <h4 className="text-success font-weight-bold">Multiple Files List</h4>
+             {multipleFiles.map((element, index) =>
+                <div key={element._id}>
+                    <h6 className="text-danger font-weight-bold">{element.title}</h6>
+                    <div className="row">
+                      {element.files.map((file, index) =>
+                        <div className="col-6">
+                            <div className="card mb-2 border-0 p-0">
+                              <img src={`https://localhost/${file.filePath}`} height="200" className="card-img-top img-responsive" alt="img"/>
+                              </div>
+                          </div>
+                       )}
+                      </div>
+                </div>
+              )}
+           </div>
+         </div>
+       </div>
+    </>
   );
 }
 
